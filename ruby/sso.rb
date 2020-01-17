7f405b19753f5b3a486c1cb0d227491ea60e111c
Pierre Laburthe <laburthe35@gmail.com>
2020-01-17 18:53:09 +0100

    Update sso.rb

--- ruby/sso.rb
+++ ruby/sso.rb
@@ -10,7 +10,7 @@ LOGORA_PUBLIC_KEY = '<YOUR_PUBLIC_KEY>'
 def get_logora_sso(user)
     # create a JSON packet of our data attributes
     data =  {
-      'id' => user['id'],
+      'uid' => user['uid'],
       'first_name' => user['first_name'],
       'last_name' => user['last_name'],
       'email' => user['email']
